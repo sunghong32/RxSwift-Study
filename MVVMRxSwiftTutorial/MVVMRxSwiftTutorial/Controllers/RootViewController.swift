c099ce90d02ed1735d36598d1b1ad713e7a03a63
민성홍 <sunghong32@naver.com>
2021-07-19 18:37:41 +0900

    configureUI addSubView

--- MVVMRxSwiftTutorial/MVVMRxSwiftTutorial/Controllers/RootViewController.swift
+++ MVVMRxSwiftTutorial/MVVMRxSwiftTutorial/Controllers/RootViewController.swift
@@ -15,6 +15,16 @@ class RootViewController: UIViewController {
     let disposeBag = DisposeBag()
     let viewModel: RootViewModel
 
+    private lazy var collectionView: UICollectionView = {
+        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
+        cv.delegate = self
+        cv.dataSource = self
+
+        cv.backgroundColor = .systemBackground
+        
+        return cv
+    }()
+
     private var articleViewModel = BehaviorRelay<[ArticleViewModel]>(value: [])
     var articleViewModelObserver: Observable<[ArticleViewModel]> {
         return articleViewModel.asObservable()
@@ -34,6 +44,7 @@ class RootViewController: UIViewController {
         super.viewDidLoad()
 
         configureUI()
+        configureCollectionView()
         fetchArticles()
         subscribe()
     }
@@ -41,6 +52,19 @@ class RootViewController: UIViewController {
     // MARK: Configures
     func configureUI() {
         view.backgroundColor = .systemBackground
+
+        self.title = self.viewModel.title
+
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        collectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        collectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+
+    func configureCollectionView() {
+        self.collectionView.register(ArticleCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     }
 
     // MARK: Helpers
@@ -56,8 +80,30 @@ class RootViewController: UIViewController {
         self.articleViewModelObserver
             .subscribe(onNext: { articles in
                 // collectionView reload
-                print(articles)
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
             })
             .disposed(by: disposeBag)
+    }
+}
+
+extension RootViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.articleViewModel.value.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ArticleCollectionViewCell
+
+        cell.imageView.image = nil
+
+        let articleViewModel = self.articleViewModel.value[indexPath.row]
+        cell.viewModel.onNext(articleViewModel)
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 120)
     }
 }
